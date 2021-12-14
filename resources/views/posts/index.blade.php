c14b89a9fbcc546b3e4f0731dff770c0ee0eaac1
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2021-12-14 21:45:35 +0800

    12/14/21

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('background')
 <!--- Start Landing Page Image -->
-    <div class="home-inner" id="admin_background">
-    </div>
+<div class="home-inner" id="admin_background">
+</div>
 <!--- End Landing Page Image -->
 @endsection
 
@@ -18,9 +18,9 @@
 
 <div class="card card-default" id="admin_card">
     @if($posts->count() > 0 )
-        <div class="card-header text-white" id="admin_card_heading">Posts</div>
+    <div class="card-header text-white" id="admin_card_heading">Posts</div>
     @else
-        <div class="card-header text-white" id="admin_card_heading">Empty</div>
+    <div class="card-header text-white" id="admin_card_heading">Empty</div>
     @endif
 
 
@@ -28,61 +28,61 @@
 
         <!-- Start Display the posts -->
         @if($posts->count() > 0 )
-            <div class="table-responsive">
+        <div class="table-responsive">
             <table class="table text-white">
-                    <thead>
-                        <th>IMAGE</th>
-                        <th>TITLE</th>
-                        <th>CATEGORY</th>
-                        <th></th>
-                        <th></th>
-                    </thead>
-                    <tbody>
-                        @foreach($posts as $post)
-                            <tr>
-                                <td>
-                                    <img src="{{ asset('/storage/' . $post->image) }}" width="120px" height="60" alt="" >
-                                </td>
-                                <td>
-                                    {{ $post->title }}
-                                </td>
-                                <td>
-                                    <!-- Redirect to edit this category -->
-                                    <a class="text-white decoration-none" href="{{ route('categories.edit', $post->category->id) }}">
-                                        {{ $post->category->name }}
-                                    </a>
-                                </td>
-                                <!-- Add restore button if not trashed -->
-                                @if($post->trashed())
-                                    <td>
-                                        <form action="{{ route('restore-posts', $post->id) }}" method="POST">
-                                            @csrf
-                                            @method('PUT')
-                                            <button type="submit" class="btn btn-sm text-white" id="edit_restore_btn">Restore</button>
-                                        </form>
-                                    </td>
-                                <!-- Add edit button if not trashed -->
-                                @else
-                                    <td class="px-0">
-                                        <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-sm text-white" id="edit_restore_btn">Edit</a>
-                                    </td>
-                                @endif
-                                <td class="pl-1">
-                                    <form action="{{ route('posts.destroy', $post->id) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-sm text-white" id="trash_delete_btn">
-                                            {{ $post->trashed() ? 'Delete' : 'Trash' }}
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-         @else
-            <h3 class="text-center text-white">No Posts Yet</h3>
+                <thead>
+                    <th>IMAGE</th>
+                    <th>TITLE</th>
+                    <th>CATEGORY</th>
+                    <th></th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    @foreach($posts as $post)
+                    <tr>
+                        <td>
+                            <img src="{{ asset('/storage/' . $post->image) }}" width="120px" height="60" alt="">
+                        </td>
+                        <td>
+                            {{ $post->title }}
+                        </td>
+                        <td>
+                            <!-- Redirect to edit this category -->
+                            <a class="text-white decoration-none" href="{{ route('categories.edit', $post->category->id) }}">
+                                {{ $post->category->name }}
+                            </a>
+                        </td>
+                        <!-- Add restore button if not trashed -->
+                        @if($post->trashed())
+                        <td>
+                            <form action="{{ route('restore-posts', $post->id) }}" method="POST">
+                                @csrf
+                                @method('PUT')
+                                <button type="submit" class="btn btn-sm text-white" id="edit_restore_btn">Restore</button>
+                            </form>
+                        </td>
+                        <!-- Add edit button if not trashed -->
+                        @else
+                        <td class="px-0">
+                            <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-sm text-white" id="edit_restore_btn">Edit</a>
+                        </td>
+                        @endif
+                        <td class="pl-1">
+                            <form action="{{ route('posts.destroy', $post->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-sm text-white" onclick="return confirm('Are you sure you want to delete this post?')" id="trash_delete_btn">
+                                    {{ $post->trashed() ? 'Delete' : 'Trash' }}
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        @else
+        <h3 class="text-center text-white">No Posts Yet</h3>
         @endif
         <!-- End Display the posts -->
 
